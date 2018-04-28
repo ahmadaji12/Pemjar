9d334b9ee78d9a29c43597412276ba8691ea1080
ahmadaji12 <Ikhwangaul3@gmail.com>
2018-04-28 09:37:12 +0700

    Add files via upload

---
+++ code6.5.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+#  Program Data Encoding
+#  author: mahendra.data@ub.ac.id
+#  execute: ./code6.5.py <IP>:<PORT>
+
+import socket
+import sys
+import pickle
+
+def read(payload):
+    data = pickle.loads(payload)
+    return data, payload[payload.find(b".")+1:]
+
+srv_ip, srv_port = sys.argv[1].split(":")
+srv_sockaddr = (srv_ip, int(srv_port))
+
+payload, cln_sockaddr = sock.recvfrom(2048)
+data, buffer = read(payload)
+print(data)
+print(buffer)
+payload, cln_sockaddr = sock.recvfrom(2048)
+payload = buffer + payload
+data, buffer = read(payload)
+print(data)
+print(buffer)
+
+sock.close()
